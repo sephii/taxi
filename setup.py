3dc6350893feb0dba95ac4c6e7befccca1696fe0
Sylvain Fankhauser <sylvain.fankhauser@gmail.com>
2014-05-10 11:16:38 +0200

    exclude tests from build

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import find_packages, setup
 from taxi import __version__
 
 tests_require = [
@@ -11,13 +11,13 @@ tests_require = [
 setup(
     name='taxi',
     version=__version__,
-    packages=['taxi', 'taxi.utils', 'taxi.parser', 'taxi.parser.parsers',
-              'taxi.ui'],
+    packages=find_packages(exclude=('tests','tests.*')),
     description='Taxi is a Zebra frontend',
     author='Sylvain Fankhauser',
     author_email='sylvain.fankhauser@liip.ch',
     scripts=['bin/taxi'],
     url='https://github.com/sephii/taxi',
     tests_require=tests_require,
-    test_suite='tests.runtests.suite'
+    test_suite='tests.runtests.suite',
+    include_package_data=False
 )
