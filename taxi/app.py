sephii/taxi

commit 59f25d6140225fcd1289d67c34a1c76e862cf9f6

Don't autofill entries file if file is speficied on the command line (fixes #6)

taxi/app.py
@@ -91,12 +91,15 @@ Available commands:
             os.mkdir(settings.TAXI_PATH)
 
         if options.get('file', None) is None:
+            options['forced_file'] = True
             try:
                 options['file'] = settings.get('file')
             except ConfigParser.NoOptionError:
                 raise Exception("Error: no file to parse. You must either "
                                 "define one in your config file with the "
                                 "'file' setting, or use the -f option")
+        else:
+            options['forced_file'] = False
 
         options['unparsed_file'] = os.path.expanduser(options['file'])
         options['file'] = datetime.date.today().strftime(os.path.expanduser(options['file']))

tests/commands/test_edit.py
@@ -1,0 +1,27 @@
+import tempfile
+
+from freezegun import freeze_time
+
+from taxi.models import Project, Activity
+from taxi.projectsdb import ProjectsDb
+from taxi.settings import Settings
+
+from . import CommandTestCase
+
+
+@freeze_time('2014-01-21')
+class EditCommandTestCase(CommandTestCase):
+    def test_autofill_with_specified_file(self):
+        """
+        Edit with specified date should not autofill it.
+        """
+        config = self.default_config.copy()
+        options = self.default_options.copy()
+
+        config['default']['auto_fill_days'] = '0,1,2,3,4,5,6'
+        options['file'] = self.entries_file
+
+        self.run_command('edit')
+
+        with open(self.entries_file, 'r') as f:
+            self.assertEqual(f.read(), '')

taxi/commands.py
@@ -394,7 +394,8 @@ class EditCommand(BaseTimesheetCommand):
         file.create_file(self.options['file'])
         is_top_down = None
 
-        if self.settings.get('auto_add') != Settings.AUTO_ADD_OPTIONS['NO']:
+        if (self.settings.get('auto_add') != Settings.AUTO_ADD_OPTIONS['NO'] and
+            not self.options.get('forced_file')):
             try:
                 t = self.get_timesheet()
             except (UndefinedAliasError, ParseError):

tests/commands/__init__.py
@@ -34,12 +34,20 @@ class CommandTestCase(TestCase):
 
         self.original_zebra_remote_send_entries = ZebraRemote.send_entries
         ZebraRemote.send_entries = Mock(side_effect=zebra_remote_send_entries)
+        self.stdout = StringIO()
+
+        _, self.config_file = tempfile.mkstemp()
+        _, self.entries_file = tempfile.mkstemp()
+        _, self.projects_db = tempfile.mkstemp()
+
         self.default_config = {
             'default': {
                 'site': 'https://zebra.liip.ch',
                 'username': 'john.doe',
                 'password': 'john.doe',
                 'date_format': '%d/%m/%Y',
+                'editor': '/bin/false',
+                'file': self.entries_file
             },
             'wrmap': {
                 'alias_1': '123/456'
@@ -49,12 +57,7 @@ class CommandTestCase(TestCase):
         self.default_options = {
         }
 
-        self.stdout = StringIO()
-        _, self.config_file = tempfile.mkstemp()
-        _, self.entries_file = tempfile.mkstemp()
-        _, self.projects_db = tempfile.mkstemp()
         self.default_options['config'] = self.config_file
-        self.default_options['file'] = self.entries_file
         self.default_options['stdout'] = self.stdout
         self.default_options['projects_db'] = self.projects_db
 

tests/commands/test_autofill.py
@@ -12,12 +12,11 @@ class AutofillCommandTestCase(CommandTestCase):
         self.run_command('autofill', config_options=config_options)
 
     def test_autofill_bottom(self):
-        config_options = {
-            'default': {
-                'auto_fill_days': '1',
-                'auto_add': 'bottom'
-            }
-        }
+        config_options = self.default_config.copy()
+        config_options['default'].update({
+            'auto_fill_days': '1',
+            'auto_add': 'bottom'
+        })
 
         self.run_autofill_command(config_options)
         entries_file_contents = self.read_entries()
@@ -33,12 +32,11 @@ class AutofillCommandTestCase(CommandTestCase):
 """)
 
     def test_autofill_top(self):
-        config_options = {
-            'default': {
-                'auto_fill_days': '1',
-                'auto_add': 'top'
-            }
-        }
+        config_options = self.default_config.copy()
+        config_options['default'].update({
+            'auto_fill_days': '1',
+            'auto_add': 'top'
+        })
 
         self.run_autofill_command(config_options)
         entries_file_contents = self.read_entries()
@@ -54,12 +52,11 @@ class AutofillCommandTestCase(CommandTestCase):
 """)
 
     def test_autofill_existing_entries(self):
-        config_options = {
-            'default': {
-                'auto_fill_days': '1',
-                'auto_add': 'top'
-            }
-        }
+        config_options = self.default_config.copy()
+        config_options['default'].update({
+            'auto_fill_days': '1',
+            'auto_add': 'top'
+        })
 
         self.write_entries("15/02/2012\n\n07/02/2012")
 

commit 05f8c33501845e39638ff046f6a7f4bae52b7cd0

Fix autofill that was broken by 59f25d6140225fcd1289d67c34a1c76e862cf9f6

taxi/app.py
@@ -95,7 +95,7 @@ Available commands:
             os.mkdir(settings.TAXI_PATH)
 
         if options.get('file', None) is None:
-            options['forced_file'] = True
+            options['forced_file'] = False
             try:
                 options['file'] = settings.get('file')
             except ConfigParser.NoOptionError:
@@ -103,7 +103,7 @@ Available commands:
                                 "define one in your config file with the "
                                 "'file' setting, or use the -f option")
         else:
-            options['forced_file'] = False
+            options['forced_file'] = True
 
         options['unparsed_file'] = os.path.expanduser(options['file'])
         options['file'] = expand_filename(options['file'])

tests/commands/test_edit.py
@@ -1,10 +1,4 @@
-import tempfile
-
 from freezegun import freeze_time
-
-from taxi.models import Project, Activity
-from taxi.projectsdb import ProjectsDb
-from taxi.settings import Settings
 
 from . import CommandTestCase
 
@@ -21,7 +15,7 @@ class EditCommandTestCase(CommandTestCase):
         config['default']['auto_fill_days'] = '0,1,2,3,4,5,6'
         options['file'] = self.entries_file
 
-        self.run_command('edit')
+        self.run_command('edit', options=options)
 
         with open(self.entries_file, 'r') as f:
             self.assertEqual(f.read(), '')