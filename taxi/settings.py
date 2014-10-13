sephii/taxi

commit 759ff0def3832189e0301a75f005a132a607380d

Set auto_fill_days default to all the days of the week

taxi/settings.py
@@ -15,7 +15,7 @@ class Settings:
     }
 
     DEFAULTS = {
-            'auto_fill_days': '',
+            'auto_fill_days': '1,2,3,4,5',
             'date_format': '%d/%m/%Y',
             'auto_add': 'auto',
             'nb_previous_files': '1',

commit 49037d3a30926b540263a7c3cf54c5845a690255

Fix default value for auto_fill_days to all working days (fixes #57)
Bug caused by 759ff0def3832189e0301a75f005a132a607380d

taxi/settings.py
@@ -15,7 +15,7 @@ class Settings:
     }
 
     DEFAULTS = {
-            'auto_fill_days': '1,2,3,4,5',
+            'auto_fill_days': '0,1,2,3,4',
             'date_format': '%d/%m/%Y',
             'auto_add': 'auto',
             'nb_previous_files': '1',