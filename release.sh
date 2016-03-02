d825774dd66684f07389ef08894b63ea3c982cac
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2016-03-02 14:33:51 +0100

    Fix upload to PyPI in release script

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ release () {
     git commit -m "Bump version number to $1" taxi/__init__.py
     git tag -m "Release $1" -s $1
     git push origin $1
-    ./setup.py sdist release
+    ./setup.py sdist upload
 }
 
 if [ "$#" -ne 1 ]; then
