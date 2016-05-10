88ae04df095fb58931b030b0af91789dcd201b4a
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2016-05-10 08:49:48 +0200

    Try to fix docs building

--- doc/conf.py
+++ doc/conf.py
@@ -12,12 +12,15 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-from taxi import __version__
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+from taxi import __version__
 
 # -- General configuration ------------------------------------------------
 
