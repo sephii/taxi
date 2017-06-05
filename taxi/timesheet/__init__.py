edd8a11ff7cf5e5719484b7ef9a0b54c0591d4bb
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2017-06-05 13:51:00 +0200

    Add docs to Timesheet class

--- taxi/timesheet/__init__.py
+++ taxi/timesheet/__init__.py
@@ -13,13 +13,53 @@ from .entry import AggregatedTimesheetEntry, EntriesCollection
 from .parser import EntryLine, TimesheetParser
 
 
+def is_entry_ignored(entry):
+    """
+    Return `True` if the given `entry` has the entry flag or is not in the aliases database.
+    """
+    return entry.ignored or entry.alias not in aliases_database
+
+
+def round_to_quarter(start_time, end_time):
+    """
+    Return the duration between `start_time` and `end_time` :class:`datetime.time` objects, rounded to 15 minutes.
+    """
+    # We don't care about the date (only about the time) but Python
+    # can substract only datetime objects, not time ones
+    today = datetime.date.today()
+    start_date = datetime.datetime.combine(today, start_time)
+    end_date = datetime.datetime.combine(today, end_time)
+
+    difference_minutes = (end_date - start_date).seconds / 60
+    remainder = difference_minutes % 15
+    # Round up
+    difference_minutes += 15 - remainder if remainder > 0 else 0
+
+    return (
+        start_date + datetime.timedelta(minutes=difference_minutes)
+    ).time()
+
+
 class Timesheet(object):
+    """
+    A timesheet is a link between an entries collection and a file.
+    """
     def __init__(self, entries=None, file=None):
         self.entries = entries if entries is not None else EntriesCollection(TimesheetParser())
         self.file = file
 
-    def get_filtered_entries(self, date=None, filter_callback=None,
-                             regroup=False):
+    def get_filtered_entries(self, date=None, filter_callback=None, regroup=False):
+        """
+        Return the timesheet entries as a dict of {:class:`datetime.date`: :class:`~taxi.timesheet.lines.EntryLine`}
+        items.
+
+        `date` can either be a single :class:`datetime.date` object to filter only entries from the given date,
+        or a tuple of :class:`datetime.date` objects representing `(from, to)`. `filter_callback` is a function that,
+        given a :class:`~taxi.timesheet.lines.EntryLine` object, should return True to include that line, or False to
+        exclude it. If `regroup` is set to True, similar entries (ie. having the same
+        :meth:`~taxi.timesheet.lines.EntryLine.hash`) will be regrouped intro a single
+        :class:`~taxi.timesheet.entry.AggregatedTimesheetEntry`.
+        """
         # Date can either be a single date (only 1 day) or a tuple for a
         # date range
         if date is not None and not isinstance(date, tuple):
@@ -94,6 +134,19 @@ class Timesheet(object):
     def get_entries(self, date=None, exclude_ignored=False,
                     exclude_unmapped=False, exclude_pushed=False,
                     regroup=False):
+        """
+        Return all entries from this timesheet as a dict of `{date: entries}` where `date` is a :class:`datetime.date`
+        object and `entries` is a list of :class:`taxi.timesheet.lines.EntryLine` instances. `date` can either be a
+        :class:`datetime.date` object or a 2-tuple of :class:`datetime.date` objects to only include entries between
+        the 2 given dates.
+
+        The `exclude_ignored`, `exclude_unmapped` and `exclude_pushed` parameters can be set to exclude certain entries
+        from the result. If `regroup` is `True`, similar entries (sharing the same alias and description) will be
+        regrouped as single entries.
+
+        This method is a helper to :meth:`get_filtered_entries`. If you need a more granular filter function, use
+        :meth:`get_filtered_entries` instead.
+        """
         def entry_filter(entry):
             return (not (exclude_ignored and entry.ignored)
                     and (not exclude_unmapped
@@ -103,13 +156,30 @@ class Timesheet(object):
 
         return self.get_filtered_entries(date, entry_filter, regroup)
 
+    def get_hours(self, **kwargs):
+        """
+        Return the total hours of the entries filtered by the given `kwargs`, which are the same as
+        :meth:`get_entries`.
+        """
+        date_entries = self.get_entries(**kwargs)
+        return sum(sum(entry.hours for entry in entries) for entries in date_entries.values())
+
     def get_ignored_entries(self, date=None, exclude_pushed=True):
+        """
+        Return all ignored entries for this timesheet. See :meth:`get_filtered_entries` for the type of the `date`
+        parameter. If `exclude_pushed` is `False`, pushed entries will be included.
+        """
         def entry_filter(entry):
-            return self.is_entry_ignored(entry) and not (exclude_pushed and entry.pushed)
+            return is_entry_ignored(entry) and not (exclude_pushed and entry.pushed)
 
         return self.get_filtered_entries(date, entry_filter)
 
     def get_non_current_workday_entries(self):
+        """
+        Return a list of :class:`~taxi.timesheet.lines.EntryLine` objects that are not considered as being in the
+        "current workday". The current workday is either the current day or the day before the current day (which will
+        be Friday if the current day is Monday).
+        """
         non_workday_entries = defaultdict(list)
 
         today = datetime.date.today()
@@ -118,15 +188,18 @@ class Timesheet(object):
         for (date, date_entries) in six.iteritems(self.entries):
             if date not in (today, yesterday) or date.strftime('%w') in [6, 0]:
                 for entry in date_entries:
-                    if not self.is_entry_ignored(entry) and not entry.pushed:
+                    if not is_entry_ignored(entry) and not entry.pushed:
                         non_workday_entries[date].append(entry)
 
         return non_workday_entries
 
-    def is_entry_ignored(self, entry):
-        return entry.ignored or entry.alias not in aliases_database
-
     def continue_entry(self, date, end_time, description=None):
+        """
+        Find the in-progress entry for the given date (ie. the last entry that has an empty end time) and set its end
+        time and optionally its description to the given values. `end_time` should be a :class:`datetime.time` object.
+        :exc:`NoActivityInProgressError` will be raised if there's no entry in the given date, if the last entry
+        doesn't have a start time, or if the last entry already has an end time.
+        """
         try:
             entry = self.entries[date][-1]
         except IndexError:
@@ -145,6 +218,11 @@ class Timesheet(object):
             entry.description = description
 
     def prefill(self, auto_fill_days, limit=None):
+        """
+        Add missing dates to the timesheet with respect to `auto_fill_days`, which should be a list of integers from
+        0-6, 0 being Monday and 6 Sunday. If `limit` is set, only dates up to the `limit` :class:`~datetime.date` will
+        be inserted.
+        """
         today = datetime.date.today()
 
         if limit is None:
@@ -162,23 +240,6 @@ class Timesheet(object):
                 self.entries[cur_date] = []
 
             cur_date = cur_date + datetime.timedelta(days=1)
-
-    @staticmethod
-    def round_to_quarter(start_time, end_time):
-        # We don't care about the date (only about the time) but Python
-        # can substract only datetime objects, not time ones
-        today = datetime.date.today()
-        start_date = datetime.datetime.combine(today, start_time)
-        end_date = datetime.datetime.combine(today, end_time)
-
-        difference_minutes = (end_date - start_date).seconds / 60
-        remainder = difference_minutes % 15
-        # Round up
-        difference_minutes += 15 - remainder if remainder > 0 else 0
-
-        return (
-            start_date + datetime.timedelta(minutes=difference_minutes)
-        ).time()
 
 
 class TimesheetCollection:
@@ -250,6 +311,9 @@ class TimesheetCollection:
             entries.update(entries_dict)
 
         return entries
+
+    def get_hours(self, **kwargs):
+        return sum(self._timesheets_callback('get_hours')(**kwargs))
 
     def __getattr__(self, name):
         """
