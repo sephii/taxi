ee96a3ae672b41ead8ae3d9fdd17c4e10ccfe1e0
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2020-08-18 08:31:40 +0200

    Upgrade Nix packages

--- pkgs.nix
+++ pkgs.nix
@@ -22,14 +22,14 @@ let
 
   package = python3Packages.buildPythonApplication rec {
     pname = "taxi";
-    version = "6.0rc2";
+    version = "6.0";
 
     # Using GitHub instead of PyPI because tests are not distributed on the PyPI releases
     src = fetchFromGitHub {
       owner = "liip";
       repo = pname;
       rev = version;
-      sha256 = "10xmm9b912vfdmwc8l4j6q7sgflk8ym7q4bj9pzj6kkv96nrgc4c";
+      sha256 = "11c27g946rgl3px5i60532hh8cscm933bpkwxl6fb29xg0q30wsy";
     };
 
     propagatedBuildInputs = [ python3Packages.click python3Packages.appdirs python3Packages.setuptools ];
@@ -49,11 +49,11 @@ let
 
   taxiZebra = python3Packages.buildPythonPackage rec {
     pname = "taxi_zebra";
-    version = "2.2.0rc2";
+    version = "2.2.0";
 
     src = python3.pkgs.fetchPypi {
       inherit pname version;
-      sha256 = "1rmvvz9pmnp03b7kh7xa6q4hgs4ccg8qdhm2zqixas69gmfpfibi";
+      sha256 = "15gr6b0xlml583kfnwynkj4i85q6jvmrczk0w939v9ip2hr3mwrn";
     };
 
     buildInputs = [ package ];
